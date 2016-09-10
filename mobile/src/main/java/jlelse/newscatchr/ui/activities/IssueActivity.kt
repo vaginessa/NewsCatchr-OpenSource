00e948ba22efa32a39420d22ca22cf56039e043d
Jan-Lukas Else <janlukas.else@gmail.com>
2016-09-10 13:41:11 +0200

    Added possibility to report issues on Github (#4)

---
+++ mobile/src/main/java/jlelse/newscatchr/ui/activities/IssueActivity.kt
@@ -0,0 +1,29 @@
+/*
+ * NewsCatchr  Copyright (C) 2016  Jan-Lukas Else
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package jlelse.newscatchr.ui.activities
+
+import com.heinrichreimersoftware.androidissuereporter.IssueReporterActivity
+import com.heinrichreimersoftware.androidissuereporter.model.github.ExtraInfo
+import com.heinrichreimersoftware.androidissuereporter.model.github.GithubTarget
+import jlelse.newscatchr.backend.helpers.Preferences
+import jlelse.newscatchr.extensions.resBool
+import jlelse.readit.R
+
+class IssueActivity : IssueReporterActivity() {
+
+    override fun getTarget() = GithubTarget("jlelse", "NewsCatchr-OpenSource")
+
+    override fun onSaveExtraInfo(extraInfo: ExtraInfo?) {
+        extraInfo?.put("debug", R.bool.debug_mode.resBool())
+        extraInfo?.put("subscribed", Preferences.supportUser)
+    }
+
+}
