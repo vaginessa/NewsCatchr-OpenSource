f42e46b4bec4b161334b9ffca78d59d4fa4ef014
Jan-Lk Else <janlukas.else@gmail.com>
2016-11-18 23:14:12 +0100

    Some progress

--- mobile/src/main/java/jlelse/newscatchr/backend/apis/ReadabilityApi.kt
+++ mobile/src/main/java/jlelse/newscatchr/backend/apis/ReadabilityApi.kt
@@ -23,24 +23,26 @@ class ReadabilityApi {
 
 	fun reparse(article: Article?): Pair<Article?, Boolean> {
 		val wordpress = wordpress(article?.url)?.filter { it.component2().notNullOrBlank() }
-		val readability = readability(article?.url)?.filter { it.component2().notNullOrBlank() }
-		val good = (wordpress?.get("title") ?: readability?.get("title")).notNullOrBlank() && (wordpress?.get("content") ?: readability?.get("content")).notNullOrBlank()
+		val mercury = mercury(article?.url)?.filter { it.component2().notNullOrBlank() }
+		val good = (wordpress?.get("title") ?: mercury?.get("title")).notNullOrBlank() && (wordpress?.get("content") ?: mercury?.get("content")).notNullOrBlank()
 		return Pair(article?.apply {
 			if (good) {
-				title = wordpress?.get("title") ?: readability?.get("title") ?: title
-				content = wordpress?.get("content") ?: readability?.get("content") ?: content
-				if ((wordpress?.get("title") ?: readability?.get("title")).notNullOrBlank()) {
+				title = wordpress?.get("title") ?: mercury?.get("title") ?: title
+				content = wordpress?.get("content") ?: mercury?.get("content") ?: content
+				if ((wordpress?.get("title") ?: mercury?.get("title")).notNullOrBlank()) {
 					enclosure = null
-					visualUrl = wordpress?.get("image") ?: readability?.get("image") ?: visualUrl
+					visualUrl = wordpress?.get("image") ?: mercury?.get("image") ?: visualUrl
 				}
 				process(true)
 			}
 		}, good)
 	}
 
-	fun readability(url: String?): Map<String, String?>? = tryOrNull(url.notNullOrBlank()) {
-		Bridge.get("https://readability.com/api/content/v1/parser?token=$ReadabilityApiKey&url=$url")
-				.asClass(ReadabilityR::class.java)
+	fun mercury(url: String?): Map<String, String?>? = tryOrNull(url.notNullOrBlank()) {
+		Bridge.get("https://mercury.postlight.com/parser?url=$url")
+				.header("Content-Type", "application/json")
+				.header("x-api-key", ReadabilityApiKey)
+				.asClass(MercuryR::class.java)
 				?.let { mapOf("title" to it.title, "content" to it.content, "image" to it.lead_image_url) }
 	}
 
@@ -55,7 +57,7 @@ class ReadabilityApi {
 		@Body var content: String? = ""
 	}
 
-	@Keep @ContentType("application/json") private class ReadabilityR : Response() {
+	@Keep @ContentType("application/json") private class MercuryR : Response() {
 		@Body var lead_image_url: String? = ""
 	}
 
