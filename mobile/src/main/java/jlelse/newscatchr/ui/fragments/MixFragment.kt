6783baf6f5873eb4d99fb83bbf252273cdab7f99
Jan-Lk Else <janlukas.else@gmail.com>
2017-02-03 19:45:11 +0100

    Copied Async Coroutines implementation due to Kotlin API change Updated libraries Fixed another fragment

--- mobile/src/main/java/jlelse/newscatchr/ui/fragments/MixFragment.kt
+++ mobile/src/main/java/jlelse/newscatchr/ui/fragments/MixFragment.kt
@@ -8,13 +8,13 @@
  * You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+@file:Suppress("EXPERIMENTAL_FEATURE_WARNING")
+
 package jlelse.newscatchr.ui.fragments
 
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.*
-import com.mcxiaoke.koi.ext.find
+import co.metalab.asyncawait.async
 import com.mikepenz.fastadapter.commons.adapters.FastItemAdapter
 import jlelse.newscatchr.backend.Article
 import jlelse.newscatchr.backend.helpers.Tracking
@@ -22,65 +22,56 @@ import jlelse.newscatchr.backend.loaders.FeedlyLoader
 import jlelse.newscatchr.extensions.getAddedString
 import jlelse.newscatchr.extensions.notNullAndEmpty
 import jlelse.newscatchr.extensions.nothingFound
+import jlelse.newscatchr.ui.layout.RefreshRecyclerUI
 import jlelse.newscatchr.ui.recycleritems.ArticleListRecyclerItem
+import jlelse.newscatchr.ui.views.StatefulRecyclerView
 import jlelse.newscatchr.ui.views.SwipeRefreshLayout
 import jlelse.readit.R
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
+import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.find
 
-class MixFragment() : BaseFragment() {
-	private var recyclerOne: RecyclerView? = null
-	private var refreshOne: SwipeRefreshLayout? = null
-	private var fastAdapter: FastItemAdapter<ArticleListRecyclerItem>? = null
+class MixFragment : BaseFragment() {
+	private var fragmentView: View? = null
+	private val recyclerOne: StatefulRecyclerView? by lazy { fragmentView?.find<StatefulRecyclerView>(R.id.refreshrecyclerview_recycler) }
+	private var fastAdapter = FastItemAdapter<ArticleListRecyclerItem>()
+	private val refreshOne: SwipeRefreshLayout? by lazy { fragmentView?.find<SwipeRefreshLayout>(R.id.refreshrecyclerview_refresh) }
 	private var feedId: String? = null
 	private var articles: Array<Article>? = null
-	private var savedInstanceState: Bundle? = null
 	private var feedlyLoader: FeedlyLoader? = null
 
 	override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		super.onCreateView(inflater, container, savedInstanceState)
-		this.savedInstanceState = savedInstanceState
-		val view = inflater?.inflate(R.layout.refreshrecycler, container, false)
+		fragmentView = fragmentView ?: RefreshRecyclerUI().createView(AnkoContext.create(context, this))
 		setHasOptionsMenu(true)
-		recyclerOne = view?.find<RecyclerView>(R.id.recyclerOne)?.apply {
-			layoutManager = LinearLayoutManager(context)
-		}
-		refreshOne = view?.find<SwipeRefreshLayout>(R.id.refreshOne)?.apply {
-			setOnRefreshListener {
-				loadArticles(false)
-			}
+		refreshOne?.setOnRefreshListener {
+			loadArticles(false)
 		}
 		feedId = getAddedString("feedId")
 		feedlyLoader = FeedlyLoader().apply {
 			type = FeedlyLoader.FeedTypes.MIX
 			feedUrl = feedId
 		}
+		if (recyclerOne?.adapter == null) recyclerOne?.adapter = fastAdapter
 		loadArticles(true)
 		Tracking.track(type = Tracking.TYPE.MIX, url = feedId)
-
-		return view
+		return fragmentView
 	}
 
-	private fun loadArticles(cache: Boolean) {
+	private fun loadArticles(cache: Boolean = false) = async {
 		refreshOne?.showIndicator()
-		doAsync {
-			if (articles == null || !cache) articles = feedlyLoader?.items(cache)
-			uiThread {
-				if (articles.notNullAndEmpty()) {
-					fastAdapter = FastItemAdapter<ArticleListRecyclerItem>()
-					recyclerOne?.adapter = fastAdapter
-					fastAdapter?.setNewList(mutableListOf<ArticleListRecyclerItem>())
-					articles?.forEach {
-						fastAdapter?.add(ArticleListRecyclerItem().withArticle(it).withFragment(this@MixFragment))
-					}
-					fastAdapter?.withSavedInstanceState(savedInstanceState)
-				} else {
-					context.nothingFound()
-					fragmentNavigation.popFragment()
-				}
-				refreshOne?.hideIndicator()
+		if (articles == null || !cache) await { articles = feedlyLoader?.items(cache) }
+		if (articles.notNullAndEmpty()) {
+			fastAdapter.clear()
+			articles?.forEach {
+				fastAdapter.add(ArticleListRecyclerItem().withArticle(it).withFragment(this@MixFragment))
 			}
+			if (cache) recyclerOne?.restorePosition()
+		} else {
+			fastAdapter.clear()
+			context.nothingFound()
+			fragmentNavigation.popFragment()
 		}
+		refreshOne?.hideIndicator()
 	}
 
 	override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
@@ -89,19 +80,12 @@ class MixFragment() : BaseFragment() {
 	}
 
 	override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-		when (item?.itemId) {
+		return when (item?.itemId) {
 			R.id.refresh -> {
 				loadArticles(false)
-				return true
+				true
 			}
-			else -> {
-				return super.onOptionsItemSelected(item)
-			}
+			else -> super.onOptionsItemSelected(item)
 		}
-	}
-
-	override fun onSaveInstanceState(outState: Bundle?) {
-		fastAdapter?.saveInstanceState(outState)
-		super.onSaveInstanceState(outState)
 	}
 }
