4557d2f64110537f334a15aec17196a55b2ee908
Jan-Lk Else <janlukas.else@gmail.com>
2017-06-10 11:35:35 +0200

    Small fix

--- mobile/src/main/java/jlelse/newscatchr/ui/layout/RefreshRecyclerUI.kt
+++ mobile/src/main/java/jlelse/newscatchr/ui/layout/RefreshRecyclerUI.kt
@@ -19,6 +19,7 @@
 package jlelse.newscatchr.ui.layout
 
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import jlelse.newscatchr.extensions.swipeRefreshLayout
 import jlelse.readit.R
@@ -33,7 +34,7 @@ class RefreshRecyclerUI : AnkoComponent<ViewManagerView> {
 			id = R.id.refreshrecyclerview_refresh
 			recyclerView {
 				id = R.id.refreshrecyclerview_recycler
-				layoutManager = LinearLayoutManager(context)
+				layoutManager = LinearLayoutManager(context) as RecyclerView.LayoutManager?
 			}
 		}
 	}
